30fcd206e76eb48eba6daa342737edaa538d6164
Gerkin <nihilivin@gmail.com>
2018-05-25 13:26:46 +0200

    Created pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './pages/index/index.component';
+import { ApiComponent } from './pages/api/api.component';
+import { TutorialsComponent } from './pages/tutorials/tutorials.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    ApiComponent,
+    TutorialsComponent
   ],
   imports: [
     BrowserModule
