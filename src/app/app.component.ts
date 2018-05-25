f5d19e006dcf51281a51d29bc031da9185a85b74
Gerkin <nihilivin@gmail.com>
2018-05-25 19:54:31 +0200

    Done header v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, HostListener } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+	private title = 'app';
+
+	constructor(private el: ElementRef) {
+		this.doScroll();
+	}
+
+	@HostListener('document:scroll')
+	doScroll() {
+		if (window.scrollY === 0) {
+			this.el.nativeElement.classList.add('attop');
+		} else {
+			this.el.nativeElement.classList.remove('attop');
+		}
+	}
 }
