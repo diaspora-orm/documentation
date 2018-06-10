1c06567d42739c88046c6b75049b59d37cc97218
Gerkin <nihilivin@gmail.com>
2018-06-11 00:06:49 +0200

    Added angular patch & vendorSync npm scripts

---
+++ vendorSync.sh
@@ -0,0 +1,8 @@
+VENDOR_DIR="vendor/typedoc-default-themes/src/default/assets"
+COMPONENT_VENDOR_DIR="src/app/pages/api/symbol/vendor/"
+
+rm -r $COMPONENT_VENDOR_DIR
+mkdir -p $COMPONENT_VENDOR_DIR
+cp -R $VENDOR_DIR/css/setup/ $COMPONENT_VENDOR_DIR/scss
+find $COMPONENT_VENDOR_DIR/scss -type f -name '*.s?ss' -print0 | xargs -0 sed -e 's:../images:/assets/images/typedoc:g' -i'.bak'
+cp -R $VENDOR_DIR/images/ src/assets/images/typedoc;
