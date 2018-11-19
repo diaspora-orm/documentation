4f0d97d7addf89d98062a448ced18b3c84f93b42
Gerkin <nihilivin@gmail.com>
2018-11-19 22:11:33 +0100

    Diaspora doc version sync script to ESM

--- refreshDiasporaDocVersion.js
+++ refreshDiasporaDocVersion.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
-const path = require('path');
-var compareVersions = require('compare-versions');
+import { writeFileSync, readdirSync, readFileSync } from 'fs';
+import { join } from 'path';
+import compareVersions from 'compare-versions';
 
 const diasporaPath = '../diaspora';
 const docPath = './src/assets/content/api';
@@ -9,19 +9,19 @@ const environmentPath = './src/environments';
 try{
 	const {version} = require(`${diasporaPath}/package.json`);
 	const doc = require(`${diasporaPath}/doc.json`);
-	fs.writeFileSync(`${docPath}/${version}.json`, JSON.stringify(doc));
+	writeFileSync(`${docPath}/${version}.json`, JSON.stringify(doc));
 
-	const docFiles = fs.readdirSync(docPath);
+	const docFiles = readdirSync(docPath);
 	const matcher = /(\/\*\s*<doc-versions>\s*\*\/)(.*?)(\/\*\s*<\/doc-versions>\s*\*\/)/;
 	const docFilesJson = JSON.stringify(docFiles.map(version => version.replace('.json', '')).sort(compareVersions).reverse()).replace(/"/g, "'");
 
-	fs.readdirSync(environmentPath).forEach(file => {
-		const envFileContent = fs.readFileSync(path.join(environmentPath, file), 'UTF-8');
+	readdirSync(environmentPath).forEach(file => {
+		const envFileContent = readFileSync(join(environmentPath, file), 'UTF-8');
 		const match = envFileContent.match(matcher);
 		if(match){
 			console.info(`Updating env file ${file}`);
 			const replacedEnvFileContent = envFileContent.replace(match[0], `${match[1]}${docFilesJson}${match[3]}`);
-			fs.writeFileSync(path.join(environmentPath, file), replacedEnvFileContent);
+			writeFileSync(join(environmentPath, file), replacedEnvFileContent);
 		} else {
 			console.warn(`Env file ${file} did not match any replacement tag`);
 		}
